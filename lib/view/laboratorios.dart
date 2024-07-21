17e2bd347b3a4dea445cd7408aa9c19728c57559
jharolsv7 <jharolsteven2002@gmail.com>
2024-07-21 01:50:17 -0500

    creacion de tres vistas: laboratorios, horarios y agendar

--- lib/view/laboratorios.dart
+++ lib/view/laboratorios.dart
@@ -1,1 +1,96 @@
+import 'package:flutter/material.dart';
 
+class Laboratorios extends StatefulWidget {
+  const Laboratorios({super.key});
+
+  @override
+  State<Laboratorios> createState() => _LaboratoriosState();
+}
+
+class _LaboratoriosState extends State<Laboratorios> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage:
+                  AssetImage('assets/images/user.jpg'), // add your image path
+            ),
+            SizedBox(height: 10),
+            Text(
+              'USUARIO',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Laboratorios SW',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // background color
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/horarios');
+              },
+              child: Text(
+                'VER HORARIOS',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // background color
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/agendar');
+              },
+              child: Text(
+                'AGENDAR LAB',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple, // background color
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              ),
+              onPressed: () {},
+              child: Text(
+                '     SCAN QR     ',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
